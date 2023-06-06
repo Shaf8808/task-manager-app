e2351a66e59468d32fdcdc50eaf0b2f80f4319e5
Shaf8808 <shadmankhan08@gmail.com>
2023-06-06 10:58:37 +0000

    Add schema to the Category table and start on the Task table

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,1 +1,20 @@
 from taskmanager import db
+
+
+class Category(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
+
+
+class Task(db.Model):
+    # schema for the Task model
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self
