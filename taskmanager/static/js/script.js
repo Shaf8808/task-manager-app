70ff327baada04bc3f28171f014360fae9106009
Shaf8808 <shadmankhan08@gmail.com>
2023-06-10 11:43:36 +0000

    Add date picker and select dropdown menu init from materialize doc- script.js
    Create add_task file which is a form that displays with the appropriate options such as task name, due date etc. Implement font-awesome icons and Materialize specific classes where appropriate.
    
    Update New Task nav link to display add_task form once clicked- base.html
    
    Add button to tasks page(homepage) that, once clicked, takes user to add_task form page.
    
    Add add_task function which stores each set of data (task name, due date etc) in the database, commits it then redirects user to homepage- routes.py

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,10 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
   // Modal trigger intialization
-  document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
+
+    // Date picker init
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // Dropdown init
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
